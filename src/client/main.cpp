fdc814d446addc380e1484a44630bfcf5c165bd9
Lastv <hugo.monjoux@ensea.fr>
2018-09-25 09:02:52 +0200

    ajout du controle des arguments dans main.cpp, Bonjour le monde

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -2,6 +2,7 @@
 
 // Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
+#include "string.h"
 
 void testSFML() {
     sf::Texture texture;
@@ -14,12 +15,21 @@ void testSFML() {
 using namespace std;
 using namespace state;
 
-int main(int argc,char* argv[]) 
+int main(int argc,char* argv[])
 {
     Exemple exemple;
     exemple.setX(53);
 
-    cout << "It works !" << endl;
+    //printf("%s\n", argv[1]);
+    //printf("%d\n", argc);
+    
+    if (argc == 2) {
+      if ( !strcmp(argv[1],"hello") ){
+        cout << "Bonjour le monde!" << endl;
+      } else {
+        cout << "Veuillez tapez hello s'il vous plait." << endl;
+      }
+    }
 
     return 0;
 }
